697a1fb381fd04aa1f9e8657f746095bb9c627ad
Tomasz Latkowski <tomasz.latkowski@tcl.com>
2018-10-22 10:05:43 +0200

    added images for tests

--- tests/datasets_test/test_places.py
+++ tests/datasets_test/test_places.py
@@ -1,10 +1,12 @@
 import numpy as np
 import tensorflow as tf
-
+import os
 from datasets import places
 
 
 class TestPlacesDataset(tf.test.TestCase):
 
   def testDataset(self):
+    image_files = os.listdir("./tests/data_test")
+    images, labels = places.dataset(image_files)
     raise NotImplementedError
