75aa5f30d46ce03f9fbb395b036256884b9275ed
Tomasz Latkowski <tomasz.latkowski@tcl.com>
2018-11-19 09:50:11 +0100

    run experiment changes

--- run.py
+++ run.py
@@ -1,18 +1,27 @@
 import argparse
 
+from config import model_type
+from models import u_net_clf
+from models import u_net_clf_tpu
+
 
 def run_experiment(dataset, model):
-  pass
+  if model == model_type.ModelType.U_NET_CPU_GPU.name:
+    u_net_clf.run_u_net()
+  elif model == model_type.ModelType.U_NET_TPU.name:
+    u_net_clf_tpu.run_u_net()
+  else
+    raise NotImplementedError
 
 
 if __name__ == '__main__':
-    args_parser = argparse.ArgumentParser()
+  args_parser = argparse.ArgumentParser()
 
-    args_parser.add_argument("dataset",
-                             required=True)
+  args_parser.add_argument("dataset",
+                           required=True)
 
-    args_parser.add_argument("model",
-                             required=True)
+  args_parser.add_argument("model",
+                           required=True)
 
-    args = args_parser.parse_args()
-    run_experiment(args.dataset, args.model)
+  args = args_parser.parse_args()
+  run_experiment(args.dataset, args.model)
